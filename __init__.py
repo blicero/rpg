4e402226c966b78bec3094b4b88fb8ee364005f9
Benjamin Walkenhorst <krylon@posteo.de>
2025-03-12 18:00:52 +0100

    Add __init__.py

---
+++ __init__.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: <2025-03-12 18:00:38 krylon>
+#
+# /data/code/python/rpg/__init__.py
+# created on 12. 03. 2025
+# (c) 2025 Benjamin Walkenhorst
+#
+# This file is part of the Wetterfrosch weather app. It is distributed
+# under the terms of the GNU General Public License 3. See the file
+# LICENSE for details or find a copy online at
+# https://www.gnu.org/licenses/gpl-3.0
+
+"""
+rpg.__init__
+
+(c) 2025 Benjamin Walkenhorst
+
+Safety pig:
+
+                        _
+ _._ _..._ .-',     _.._(`))
+'-. `     '  /-._.-'    ',/
+   )         \            '.
+  / _    _    |             \
+ |  a    a    /              |
+ \   .-.                     ;
+  '-('' ).-'       ,'       ;
+     '-;           |      .'
+        \           \    /
+        | 7 .__ _.-  \   \
+        | |  |  ``/  /`  /
+       /,_|  |   /,_/   /
+          /,_/      '`-'
+
+
+"""
+
+# Local Variables: #
+# python-indent: 4 #
+# End: #
